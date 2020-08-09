e21f881631566bf9641695843492524db7363a1b
sushiyue <1020228008@qq.com>
2020-08-09 20:25:29 +0800

    ttt

--- src/main/java/com/fh/shop/api/student/Student.java
+++ src/main/java/com/fh/shop/api/student/Student.java
@@ -3,5 +3,6 @@ package com.fh.shop.api.student;
 public class Student {
     public static void main(String[] args) {
         System.out.println("----------------");
+        System.out.println("****************");
     }
 }
